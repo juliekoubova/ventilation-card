18a176616bf56d9f7fa509a44ade6a1bf4b0fc96
Julie Koubova <jkoubova@microsoft.com>
2021-11-06 23:21:24 +0100

    anim

--- ventilation.js
+++ ventilation.js
@@ -1,11 +1,16 @@
-if ('MutationObserver' in window) {
-    document.addEventListener('DOMContentLoaded', () => {
+(function () {
+    const Mapping = {
+        freshIn: 'fresh-in',
+        power: 'power',
+        preheater: 'preheater',
+    };
 
-        const Mapping = {
-            freshIn: 'fresh-in',
-            preheater: 'preheater',
-        };
-        const PreheaterThreshold = 10;
+    const PreheaterThreshold = 10;
+
+    const FanRotationDurationMax = 10; // seconds
+    const FanRotationDurationMin = 2; // seconds
+
+    function onDomContentLoaded() {
 
         /** @param {keyof Mapping} name */
         function getValue(name) {
@@ -13,13 +18,46 @@ if ('MutationObserver' in window) {
             return el ? parseInt(el.textContent, 10) : 0;
         }
 
-        const observer = new MutationObserver((mutations, observer) => {
+        function updateFanAnimation() {
+            const power = getValue('power');
+            const anim = document.getElementById('fan').querySelector('animateTransform');
+
+            if (power < 1) {
+                anim.endElement();
+            } else {
+                const currentDuration = anim.getSimpleDuration();
+                const newDuration = FanRotationDurationMax -
+                    (power / 100) * (FanRotationDurationMax - FanRotationDurationMin);
+
+                try {
+                    const currentTime = anim.getCurrentTime();
+                    const t = currentTime / currentDuration;
+                    const newTime = t * newDuration;
+
+                    anim.setAttribute('dur', `${newDuration}s`);
+                    anim.setCurrentTime(newTime);
+                } catch (e) {
+                    anim.beginElement();
+                }
+            }
+        }
+
+        function updatePreheater() {
             document.documentElement.classList.toggle(
                 'preheater',
                 getValue('preheater') - getValue('freshIn') > PreheaterThreshold
             );
+        }
+
+        const observer = new MutationObserver((mutations, observer) => {
+            updatePreheater();
+            updateFanAnimation();
         });
 
         observer.observe(document.documentElement, { characterData: true, childList: true, subtree: true })
-    });
-}
+    }
+
+    if ('MutationObserver' in window) {
+        document.addEventListener('DOMContentLoaded', onDomContentLoaded);
+    }
+})();
